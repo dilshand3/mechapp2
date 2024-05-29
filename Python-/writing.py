ac9ae593fe65f310a8042c909fbd28f8097aac0f
dilshan.d3 <dilshan72404@gmail.com>
2024-05-29 12:39:45 +0530

    these are writing programe result

---
+++ Python-/writing.py
@@ -0,0 +1,7 @@
+write = open('writing.txt','w')
+write.write("this is my first line in writing programe")
+write.write("hello dilshan u change line ")
+new = open('new.txt','w')
+new.write("this is my new file using python programe")
+write.close()
+
