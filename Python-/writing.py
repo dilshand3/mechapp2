6c77bbf182c795d484956918e62d65638a607e12
dilshan <167160416+dilshand3@users.noreply.github.com>
2024-07-01 18:00:27 +0530

    Delete Python- directory

--- Python-/writing.py
+++
@@ -1,9 +0,0 @@
-write = open('writing.txt','w')#this is for open the file and w is for the texting
-write.write("this is my first line in writing programe")
-write.write("hello dilshan u change line ")#this is for new txt
-
-new = open('new.txt','w')#this is for making new file
-new.write("this is my new file using python programe")#this is writing new txt in that files
-write.close()
-
-#this is writing files or making new files
